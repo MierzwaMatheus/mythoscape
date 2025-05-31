b42f62962b460a9706b974c4053aca1619d0b5df
MathMierzwa <matheus.oliveira@inbot.com.br>
2025-05-31 13:12:02 -0300

    fix: adiciona exportações de autenticação no firebase.ts

--- fire/src/lib/firebase.ts
+++ fire/src/lib/firebase.ts
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider, OAuthProvider } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,3 +22,19 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+// Export auth and database instances
+export const auth = getAuth(app);
+export const database = getDatabase(app);
+
+// Provedores de autenticação
+export const googleProvider = new GoogleAuthProvider();
+export const appleProvider = new OAuthProvider('apple.com');
+
+// Configurações adicionais dos provedores
+googleProvider.setCustomParameters({
+  prompt: 'select_account'
+});
+
+appleProvider.addScope('email');
+appleProvider.addScope('name');
